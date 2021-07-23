797c503f57be499460d4da75d9f6b5342d363678
Daniel-py-tst <82398893+Daniel-py-tst@users.noreply.github.com>
2021-07-23 15:39:43 -0400

    Create ValueIncrementer.lua

---
+++ ValueIncrementer.lua
@@ -0,0 +1,28 @@
+local intvalue = --your intvalue
+local TextObj = --your text label/box
+local Runservice = game:GetService("RunService")
+
+
+
+
+local l = value.Value
+intvalue.Changed:Connect(function(newVal)
+	local Difference = newVal - l 
+	l = newVal
+	local incnum = Difference/100.0
+
+	local o = 0 
+	local c c = Runservice.Heartbeat:Connect(function()
+		o += 1
+		if o <= Difference/incnum then
+			local num = TextObj.Text + incnum
+			TextObj.Text = num	
+		else
+			local remanider = l - tonumber(TextObj.Text)
+			if remanider ~= 0 then
+				TextObj.Text += remanider
+			end
+			c:Disconnect()
+		end
+	end)
+end)
